8ee7cbb5cb4cb5c04280c9683b0fdc7d0e04e983
VadymMakevytss <vadymlat@mail.ru>
2021-07-14 18:15:43 +0300

    Updates

---
+++ precache-manifest.b979e052d511f96ff3c64b818a261e0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8796acce531aa6a9ce30995ac08b3b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f14103df3cf1fcde9261",
+    "url": "./static/css/main.3f48d633.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f14103df3cf1fcde9261",
+    "url": "./static/js/main.317fed07.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
